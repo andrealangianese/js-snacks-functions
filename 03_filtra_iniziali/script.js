f8ac7ff706650cfc3fae27d7e57394eb400803d7
andrealangianese <andre.langia.01@gmail.com>
2025-12-09 15:49:54 +0100

    feat: esercizio cerca le iniziali uguali e inseriscile in un nuovo array

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -4,9 +4,28 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function firstLetterA(letter) {
+    //creo array per stampare succesivamente 
+    const result = [];
 
+    //creo il ciclo per verificare 
+    for (let i = 0; i < names.length; i++) {
+        //prendo le iniziali 
+        let check = names[i];
+        //verifico se l'iniziali sono uguali ad una determinata lettera
+        if (check[0] === letter) {
+            //se la prima lettera è uguale a quella che dichiaro sotto allora la pusha nell'array che ho creato
+            result.push(check);
+        }
+    }
+    return result
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+// stampo i nomi 
+console.log(names);
+// stampo solo le iniziali A
+console.log(firstLetterA("A"));
 
 
 
