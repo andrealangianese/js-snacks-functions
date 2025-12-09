e942aa155871f5bd832309e0efcaf2a31dd424ff
andrealangianese <andre.langia.01@gmail.com>
2025-12-09 15:09:27 +0100

    feat: iniziali stampate

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,24 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function initialLetters(array) {
+    //array dove metto le inziali
+    const result = [];
 
+    // ciclo per verificare tutti i nomi 
+    for(let i = 0; i < names.length; i++) {
+        // prendo solo l'iniziale di tutti i nomi 
+        let verify = names[i][0];
+        result.push(verify);
+    }
+    return result
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+//stampo i nomi completi
+console.log(names);
+//stampo le iniziali dei nomi
+console.log(initialLetters());
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
