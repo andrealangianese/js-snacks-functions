580fb837d92efcf873ef2bc2d41c98309468ad7e
andrealangianese <andre.langia.01@gmail.com>
2025-12-09 17:00:36 +0100

    feat: buongiorno,buonasera in base all'ora della macchina dell'utente

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,28 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+function buondi(nome) {
+    //ottengo ora dalla macchina dell'utente
+    const ora = new Date().getHours();
+    //imposto variabile per riutilizzarla sotto
+    let sayHi;
+
+    //uso condizionali per salutare utente in base all'ora in cui si trova
+    if (ora <= 13) {
+        sayHi =" buongiorno bellezza";
+    } else if (ora <= 17) {
+        sayHi = "buon pomeriggio cara"
+    } else {
+        sayHi = "buonaserata"
+    }
+    //ritorno il risultato
+    return sayHi +" "+ nome;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(buondi(name));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
